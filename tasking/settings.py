82ebd4667c1c9e17209edd130b313674fe42f690
peppuo <peppujool@hotmail.com>
2019-12-27 01:00:25 +0000

    removed django-heroku package

--- tasking/settings.py
+++ tasking/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -128,9 +127,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-# Automatic Configuration
-# https://devcenter.heroku.com/articles/django-app-configuration
-
-# django_heroku.settings(locals(), databases=False)
