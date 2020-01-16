44abb9b29a990c17f5d2c6002f9ca57d80742884
peppuo <peppujool@hotmail.com>
2020-01-16 01:27:15 +0000

    added template and view to add tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from tasks.views import render_tasks_table
+from tasks.views import TaskCreate, render_tasks_table
 
 
 urlpatterns = [
     path('', render_tasks_table, name='tasks_table'),
+    path('create/', TaskCreate.as_view(), name='tasks_create'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Tasks
 
 
 def render_tasks_table(requests):
-    return render(requests, 'tasks_table.html')
+    return render(requests, 'tasks/tasks_table.html')
 
 
-def render_add_task(requests):
-    pass
+class TaskCreate(CreateView):
+    model = Tasks
+    fields = ['name', 'category', 'status', 'description', 'duedate',
+              'importance', ]
+    success_url = reverse_lazy('')
 
 
 def render_edit_task(requests):
