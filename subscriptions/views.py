822175ff61549fd9ac342b2cff0f0683ecae843b
peppuo <peppujool@hotmail.com>
2020-01-26 21:28:01 +0000

    added checkout view

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 
 from .models import PremiumUser
 import stripe
+
 
 @login_required
 def checkout(request):
@@ -14,5 +15,24 @@ def checkout(request):
         pass
 
     if request.method == 'POST':
-        stripe_customer = stripe.PremiumUser.create(
+        stripe_customer = stripe.Customer.create(
             email=request.user.email, source=request.POST['stripeToken'])
+        subscription = stripe.Subscription.create(
+            premiumuser=stripe_customer.id)
+
+        premiumuser = PremiumUser()
+        premiumuser.user = request.user
+        premiumuser.stripe_id = stripe_customer.id
+        premiumuser.stripe_subscription_id = subscription.index
+        premiumuser.save()
+
+        return redirect('base')
+    else:
+        price = 1000
+        og_euro = 10
+        final_euro = 10
+        return render(
+            request,
+            'subscription/checkout.html',
+            {'price': price, 'og_euro': og_euro, 'final_euro': final_euro},
+        )
